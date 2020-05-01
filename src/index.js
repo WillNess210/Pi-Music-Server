d1eb582f12e27d56e47b77f729769ebf4afdf19b
will <willness210@gmail.com>
2020-05-01 17:03:39 -0600

    startup sound

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import StartupSound from '../public/startup-sound.mp3'
 import './index.css';
+
+let audio = new Audio(StartupSound);
+audio.play();
 
 ReactDOM.render(
   <App />,
