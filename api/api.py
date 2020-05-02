b42ede8da5f1de0650ba0713e5a0a3d9c333949b
will <willness210@gmail.com>
2020-05-01 22:08:47 -0600

    added bg process test

--- api/api.py
+++ api/api.py
@@ -1,8 +1,24 @@
 import time
+import threading
 from flask import Flask
 import pygame
 
 app = Flask(__name__)
+
+REFRESH_TIME = 5
+
+dataLock = threading.lock()
+
+yourThread = threading.Thread()
+
+def func_to_repeat():
+    print("Eyy")
+    yourThread = threading.Timer(REFRESH_TIME, func_to_repeat)
+    yourThread.start()
+
+yourThread = threading.Timer(REFRESH_TIME, func_to_repeat)
+yourThread.start()
+
 
 @app.route('/time')
 def get_current_time():
@@ -15,3 +31,16 @@ def streamwav():
     pygame.mixer.music.load('../public/startup-sound.wav')
     pygame.mixer.music.play()
     return {'success': True};
+
+
+def createMusicObject(platform, song_id):
+    return {
+            "platform": platform,
+            "song_id": song_id
+            }
+
+song_queue = []
+
+@app.route("/addSongToQueue/<platform>/<song_id>")
+def addSongToQueue(platform, song_id):
+    song_queue.append(createMusicObject(platform, song_id))
