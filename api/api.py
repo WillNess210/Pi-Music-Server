4d3ae170a46368155dde7870a8abc3459bf2ad77
will <willness210@gmail.com>
2020-05-01 18:04:42 -0600

    added startup

--- api/api.py
+++ api/api.py
@@ -1,7 +1,14 @@
 import time
+from flask import Flask
+from playsound import playsound
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time' : time.time()}
+
+
+@app.route("/wav")
+def streamwav():
+    playsound('../public/startup-sound.mp3')
--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from '../public/logo.svg';
+import React, { useState, useEffect } from 'react';
+//import logo from '../public/logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        ... no changes in this part ...
+
+        <p>The current time is {currentTime}.</p>
+      </header>
+      <button onClick={() => fetch('/wav')}>Play Sound On Server</button>
+    </div>
+  );
 }
 
 export default App;
