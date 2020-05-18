8aa272e0a6bfbb11f94dc4a845a66772a0277643
will <willness210@gmail.com>
2020-05-18 10:34:01 -0600

    improved header look and storage

--- music_controller_webserver/src/components/layout/Header.js
+++ music_controller_webserver/src/components/layout/Header.js
@@ -2,10 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(){
+    const linkData = [
+        {
+            title: 'Queue',
+            link: "/",
+        },
+        {
+            title: 'Soundcloud Search',
+            link: "/add",
+        },
+        {
+            title: 'Will Soundcloud',
+            link: "/will",
+        },
+    ]
     return (
         <header style={headerStyle}>
             <h1> Music Player </h1>
-            <Link style={linkStyle} to="/">Queue</Link> | <Link style={linkStyle} to="/add">Add Song</Link>
+            <div style={navStyle}>
+                {linkData.map((linkObj, i) => {
+                    return <Link className="hoverLink" style={linkStyle} to={linkObj.link}>{linkObj.title}</Link>
+                })}
+            </div>
+            
         </header>
     )
 }
@@ -17,9 +36,17 @@ const headerStyle = {
     padding: '10px'
 }
 
+const navStyle = {
+    width: "100%",
+};
+
+
 const linkStyle = {
-    color: '#fff',
-    textDecoration: 'none'
-}
+    textDecoration: 'none',
+    fontSize: "25px",
+    display: 'inline',
+    margin: '5px',
+    padding: '5px',
+};
   
   export default Header;
