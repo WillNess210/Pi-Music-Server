9e0d1d8080d3e824f11b9ec2306f132abed147d2
will <willness210@gmail.com>
2020-05-18 16:40:33 -0600

    updated startup_flask script to use new command

--- startup_flask.sh
+++ startup_flask.sh
@@ -1,3 +1,3 @@
 /bin/sleep 15
-cd /home/pi/Documents/Pi-Music-Server/music_backend
-sudo su pi -c "python3 music_api.py"
+cd /home/pi/Documents/Pi-Music-Server
+sudo su pi -c "python3 -m music_backend.music_api."
