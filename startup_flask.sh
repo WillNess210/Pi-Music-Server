03184f1f4aadd7e8197096acd8fa54130e8a436b
will <willness210@gmail.com>
2020-05-21 14:47:42 -0600

    improved startup scripts

--- startup_flask.sh
+++ startup_flask.sh
@@ -1,4 +1,8 @@
-/bin/sleep 20
 cd /home/pi/Documents/Pi-Music-Server
+git pull
+cd music_controller_webserver
+npm i
+npm start &
+cd ..
 sudo su pi -c "python3 -m music_backend.music_api"
 /bin/sleep 20
--- startup_react.sh
+++ startup_react.sh
@@ -1,4 +1,1 @@
-cd /home/pi/Documents/Pi-Music-Server
-git pull
-cd /home/pi/Documents/Pi-Music-Server/music_controller_webserver
-npm start
+echo "no longer in use"
