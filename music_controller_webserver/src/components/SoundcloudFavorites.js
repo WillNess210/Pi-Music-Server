e5e8749f4c8a846bd6a70bc1cf32460c3f5fe305
will <willness210@gmail.com>
2020-05-21 00:35:18 -0600

    added infinite scroll feature

--- music_controller_webserver/src/components/SoundcloudFavorites.js
+++ music_controller_webserver/src/components/SoundcloudFavorites.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component , Fragment} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,9 +7,37 @@ import CustomItem from './CustomItem';
 
 class SoundcloudFavorites extends Component{
 
-    state = {
-        songs: [],
-    };
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            songs: [],
+            all_songs: [],
+            hasMore: true,
+            isLoading: false,
+            error: false,
+            cur_index: 0,
+        };
+
+        window.onscroll = () => {
+            const{
+                loadSongs,
+                state: {
+                    error,
+                    isLoading,
+                    hasMore,
+                },
+            } = this;
+    
+            if(error || isLoading || !hasMore) return;
+    
+            if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+                loadSongs();
+            }
+        };
+    }
+
+    
 
     addSong = (track_url) => {
         // remove song locally
@@ -25,15 +53,40 @@ class SoundcloudFavorites extends Component{
         this.props.add_song(chosen_song.url);
     }
 
-    componentDidMount(){
+    componentWillMount(){
         this.setState({
-            songs: this.props.songs,
+            songs: [],
+            all_songs: this.props.songs,
+            hasMore: this.props.songs.length > 0,
+            isLoading: false,
+            error: false,
+            cur_index: 0,
+        })
+        this.loadSongs()
+    }
+
+    loadSongs = () => {
+        console.log('Loading Songs');
+        this.setState({ isLoading : true }, () => {
+            const nextSongs = this.state.all_songs.splice(this.state.cur_index, 20);
+            if(nextSongs.length == 0){
+                this.setState({hasMore: false});
+                console.log('Ran out of Favorited Songs');
+                return;
+            }
+            const nextStateSongs = [...this.state.songs, ...nextSongs];
+            this.setState({ 
+                cur_index : this.state.cur_index + 20,
+                isLoading : false,
+                songs: nextStateSongs,
+            });
         })
     }
+    
 
     render(){
         let shuffle_item = <CustomItem func={this.addRandom} text={'Add Random Song From Likes'}/>; //<ShuffleItem add_random_song={this.addRandom}/>;
-        return <SongQueue queue_type='add' songs={this.state['songs']} current_song={null} song_mod={this.addSong} prefix_songitem={shuffle_item}/>
+        return <SongQueue queue_type='add' songs={this.state.songs} current_song={null} song_mod={this.addSong} prefix_songitem={shuffle_item}/>
     }
 
 }
